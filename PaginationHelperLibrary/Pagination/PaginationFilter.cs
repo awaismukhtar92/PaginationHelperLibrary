a6ab24c8debaa0f26a5500186343f0383edfc309
Awais <awais@asimplify.com>
2021-11-15 17:56:40 +0500

    PageSize limit to 30

--- PaginationHelperLibrary/Pagination/PaginationFilter.cs
+++ PaginationHelperLibrary/Pagination/PaginationFilter.cs
@@ -17,7 +17,7 @@ namespace PaginationHelper.Pagination
         public PaginationFilter(int pageNumber, int pageSize, Dictionary<string, string> filter =null)
         {
             this.PageNumber = pageNumber < 1 ? 1 : pageNumber;
-            this.PageSize = pageSize > 10 ? 10 : pageSize;
+            this.PageSize = pageSize > 30 ? 30 : pageSize;
             this.Filter = filter;
         }
     }
